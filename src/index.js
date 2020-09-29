7fd9b1bca20121c4e64532cbcfed4f749bb2adbf
Nick Petrenko <extendsnull@gmail.com>
2020-09-29 11:27:38 +0300

    init: start task and add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (Array.isArray(array) && array.length) ? Math.min(...array): 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (Array.isArray(array) && array.length) ? Math.max(...array): 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    const reducer = function (accumulator, current) {
+        return accumulator + current;
+    };
+    return (Array.isArray(array) && array.length) ? array.reduce(reducer) / array.length: 0;
 }
